002a9358cdecef179a2abee5c94bcdc6ac7cdb62
Aida Abdikulova <abdikulova@gmail.com>
2019-06-27 03:34:44 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,10 @@
 # Add  code here!
 def prime?(number)
   empty_array = [1..number]
-  if empty_array.collect do |value|
+  empty_array.collect do |value|
     value % number == 0
     return true
   else
     false
   end
-end
 end
