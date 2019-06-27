b268bb7bad4e80436c2b10532ad023efe820ac11
Aida Abdikulova <abdikulova@gmail.com>
2019-06-27 03:49:09 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,5 @@
 # Add  code here!
 def prime?(number)
-  empty_array = [2...number]
-  empty_array.all?{|value| number % value == 0}
+  empty_array = [*2...number]
+  empty_array.none?{|value| number % value == 0}
 end
