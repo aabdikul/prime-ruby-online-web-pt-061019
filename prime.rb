7fea896a1d80e800251e3a5102b9c5bf2c456de5
Aida Abdikulova <abdikulova@gmail.com>
2019-06-27 03:41:23 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,10 @@
 # Add  code here!
 def prime?(number)
-  empty_array = [*1...10]
+  empty_array = [*1...number]
   empty_array.collect do |value|
     value % number == 0
     puts empty_array
   end
 end
+
+prime?(10)
